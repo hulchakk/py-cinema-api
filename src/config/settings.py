aa1d717b509e6a64cfee7aa723c2cbb74dc91ab6
Dmytro <ys33ys33ys55@gmail.com>
2026-09-08 18:26:23 +0300

    feat(config): add STRIPE_WEBHOOK_SECRET var to settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -13,6 +13,7 @@ class Settings(BaseSettings):
     LOGIN_TIME_DAYS: int = 86400
 
     STRIPE_SECRET_KEY: str = "sk_test_change_me_in_env"
+    STRIPE_WEBHOOK_SECRET: str = "whsec_change_me_in_env"
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
