f00ac2de3912521e51e18c480d82c27039e62fc9
Dmytro <ys33ys33ys55@gmail.com>
2026-09-10 12:10:29 +0300

    feat(config): add S3 storage configuration settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -15,6 +15,16 @@ class Settings(BaseSettings):
     STRIPE_SECRET_KEY: str = "sk_test_change_me_in_env"
     STRIPE_WEBHOOK_SECRET: str = "whsec_change_me_in_env"
 
+    S3_STORAGE_HOST: str = "minio-cinema"
+    S3_STORAGE_PORT: int = 9000
+    S3_STORAGE_ACCESS_KEY: str = "minio_admin"
+    S3_STORAGE_SECRET_KEY: str = "some_password"
+    S3_BUCKET_NAME: str = "cinema-storage"
+
+    @property
+    def S3_STORAGE_ENDPOINT(self) -> str:
+        return f"http://{self.S3_STORAGE_HOST}:{self.S3_STORAGE_PORT}"
+
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
