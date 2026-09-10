562843fc1682ae6f9269757e3991cd993dc45c13
Dmytro <ys33ys33ys55@gmail.com>
2026-09-10 12:42:36 +0300

    feat(storage): add delete_file method to S3StorageInterface

--- src/services/storages/interfaces.py
+++ src/services/storages/interfaces.py
@@ -17,6 +17,15 @@ class S3StorageInterface(ABC):
         pass
 
     @abstractmethod
+    async def delete_file(self, file_name: str) -> None:
+        """
+        Deletes a file from the storage.
+
+        :param file_name: The name of the file to be deleted from the bucket.
+        """
+        pass
+
+    @abstractmethod
     async def get_file_url(self, file_name: str) -> str:
         """
         Generate a public URL for a file stored in the S3-compatible storage.
