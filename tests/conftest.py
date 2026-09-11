93cef48bc4ffdcef06d6981051745041b54702f2
Dmytro <ys33ys33ys55@gmail.com>
2026-09-11 12:22:15 +0300

    feat(tests): add mock email sender and override notificator dependency

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,12 @@ from httpx import AsyncClient, ASGITransport
 from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
+from config.dependencies import get_accounts_email_notificator
 from config.settings import settings
 from database.models.base import Base
 from main import app
 from database.session import get_db
+from mocks.email import MockEmailSender
 
 db_engine = create_async_engine(settings.DB_URL, poolclass=NullPool)
 SessionLocal = async_sessionmaker(
@@ -43,3 +45,10 @@ async def reset_db_before_every_test():
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     yield
+
+
+@pytest.fixture(autouse=True)
+def override_email_dependency():
+    app.dependency_overrides[get_accounts_email_notificator] = lambda: MockEmailSender()
+    yield
+    app.dependency_overrides.pop(get_accounts_email_notificator, None)
