d3bda047d489c59f29a82cc40150af354aa83614
Dmytro <ys33ys33ys55@gmail.com>
2026-09-11 15:30:42 +0300

    test(accounts): add password reset endpoints integration tests

--- tests/integration/test_accounts.py
+++ tests/integration/test_accounts.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timedelta, timezone
+
 import pytest
 from httpx import AsyncClient, Response
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
@@ -10,6 +12,7 @@ from database.models.accounts import (
     ActivationTokenModel,
     UserGroupModel,
     RefreshTokenModel,
+    PasswordResetTokenModel,
 )
 
 
@@ -23,9 +26,10 @@ async def _get_default_user_group(db_session: AsyncSession) -> UserGroupModel:
 
 
 class TestRegisterAndActivateEndpoints:
-    user_email = "user@example.com"
     register_endpoint = "/api/v1/accounts/register"
     activate_endpoint = "/api/v1/accounts/activate"
+
+    user_email = "user@example.com"
 
     async def _register_default_user(self, client: AsyncClient) -> Response:
         response = await client.post(
@@ -162,9 +166,10 @@ class TestRegisterAndActivateEndpoints:
 
 
 class TestLoginEndpoint:
+    login_endpoint = "/api/v1/accounts/login"
+
     user_email = "user@example.com"
     user_password = "1Qazcde3"
-    login_endpoint = "/api/v1/accounts/login"
 
     async def _login_default_user(self, client: AsyncClient) -> Response:
         response = await client.post(
@@ -251,6 +256,7 @@ class TestLoginEndpoint:
 
 class TestRefreshEndpoint:
     refresh_endpoint = "/api/v1/accounts/refresh"
+
     user_email = "user@example.com"
     user_password = "1Qazcde3"
 
@@ -365,3 +371,179 @@ class TestRefreshEndpoint:
         )
 
         assert response.status_code == 400
+
+
+class TestResetPasswordEndpoints:
+    request_password_reset_endpoint = "/api/v1/accounts/password/request-reset"
+    reset_password_complete_endpoint = "/api/v1/accounts/password/reset-complete"
+
+    user_email = "user@example.com"
+    user_password = "1Qazcde3"
+    user_new_password = "1NewPassword3"
+
+    async def _create_default_user(self, db_session: AsyncSession) -> UserModel:
+        group = await _get_default_user_group(db_session)
+
+        user = UserModel.create(
+            email=self.user_email,
+            raw_password=self.user_password,
+            group_id=group.id,
+        )
+
+        user.is_active = True
+
+        db_session.add(user)
+        await db_session.commit()
+        await db_session.refresh(user)
+
+        return user
+
+    async def test_successful_request_password_reset(
+        self, client: AsyncClient, db_session: AsyncSession
+    ) -> None:
+        user = await self._create_default_user(db_session)
+
+        response = await client.post(
+            self.request_password_reset_endpoint,
+            json={
+                "email": self.user_email,
+            },
+        )
+
+        assert response.status_code == 200
+
+        stmt = select(PasswordResetTokenModel).where(
+            PasswordResetTokenModel.user_id == user.id
+        )
+        token = await db_session.scalar(stmt)
+
+        assert token is not None
+
+    async def test_request_password_reset_unexistent_user(
+        self, client: AsyncClient, db_session: AsyncSession
+    ) -> None:
+        response = await client.post(
+            self.request_password_reset_endpoint,
+            json={"email": self.user_email},
+        )
+
+        assert response.status_code == 200
+
+        stmt = select(func.count(select(PasswordResetTokenModel.id)))
+        tokens_count = await db_session.scalar(stmt)
+
+        assert tokens_count == 0
+
+    async def test_successful_password_reset(
+        self, client: AsyncClient, db_session: AsyncSession
+    ) -> None:
+        user = await self._create_default_user(db_session)
+
+        token = PasswordResetTokenModel(
+            user_id=user.id,
+        )
+
+        db_session.add(token)
+        await db_session.commit()
+        await db_session.refresh(token)
+
+        response = await client.post(
+            self.reset_password_complete_endpoint,
+            json={
+                "email": self.user_email,
+                "token": token.token,
+                "password": self.user_new_password,
+            },
+        )
+
+        assert response.status_code == 200
+
+        await db_session.refresh(user)
+
+        assert user.verify_password(self.user_new_password)
+
+    async def test_password_reset_with_invalid_token(
+        self, client: AsyncClient, db_session: AsyncSession
+    ) -> None:
+        user = await self._create_default_user(db_session)
+
+        token = PasswordResetTokenModel(
+            user_id=user.id,
+        )
+
+        db_session.add(token)
+        await db_session.commit()
+        await db_session.refresh(token)
+
+        response = await client.post(
+            self.reset_password_complete_endpoint,
+            json={
+                "email": self.user_email,
+                "token": "invalid_token",
+                "password": self.user_new_password,
+            },
+        )
+
+        assert response.status_code == 400
+
+        await db_session.refresh(user)
+
+        assert not user.verify_password(self.user_new_password)
+
+    async def test_password_reset_with_invalid_email(
+        self, client: AsyncClient, db_session: AsyncSession
+    ) -> None:
+        user = await self._create_default_user(db_session)
+
+        token = PasswordResetTokenModel(
+            user_id=user.id,
+        )
+
+        db_session.add(token)
+        await db_session.commit()
+        await db_session.refresh(token)
+
+        response = await client.post(
+            self.reset_password_complete_endpoint,
+            json={
+                "email": "invalidemail@example.com",
+                "token": token.token,
+                "password": self.user_new_password,
+            },
+        )
+
+        assert response.status_code == 400
+
+        await db_session.refresh(user)
+
+        assert not user.verify_password(self.user_new_password)
+
+    async def test_password_reset_with_expired_token(
+        self, client: AsyncClient, db_session: AsyncSession
+    ) -> None:
+        user = await self._create_default_user(db_session)
+
+        token = PasswordResetTokenModel(
+            user_id=user.id,
+        )
+
+        token.expires_at = datetime.now(timezone.utc) - timedelta(hours=9999)
+
+        db_session.add(token)
+        await db_session.commit()
+        await db_session.refresh(token)
+
+        response = await client.post(
+            self.reset_password_complete_endpoint,
+            json={
+                "email": self.user_email,
+                "token": token.token,
+                "password": self.user_new_password,
+            },
+        )
+
+        assert response.status_code == 400
+
+        await db_session.refresh(user)
+
+        assert not user.verify_password(self.user_new_password)
