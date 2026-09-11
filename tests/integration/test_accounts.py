07c85c3764ecddbc067d706dfcfc5899a229b64c
Dmytro <ys33ys33ys55@gmail.com>
2026-09-11 14:21:32 +0300

    test(accounts): add login endpoint integration tests

--- tests/integration/test_accounts.py
+++ tests/integration/test_accounts.py
@@ -1,3 +1,4 @@
+import pytest
 from httpx import AsyncClient, Response
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,7 +9,17 @@ from database.models.accounts import (
     UserGroupEnum,
     ActivationTokenModel,
     UserGroupModel,
+    RefreshTokenModel,
 )
+
+
+async def _get_default_user_group(db_session: AsyncSession) -> UserGroupModel:
+    stmt = select(UserGroupModel).where(UserGroupModel.name == UserGroupEnum.USER)
+    user_group = await db_session.scalar(stmt)
+
+    assert user_group is not None
+
+    return user_group
 
 
 class TestRegisterAndActivateEndpoints:
@@ -44,10 +55,7 @@ class TestRegisterAndActivateEndpoints:
     async def test_register_existing_user(
         self, db_session: AsyncSession, client: AsyncClient
     ) -> None:
-        stmt = select(UserGroupModel).where(UserGroupModel.name == UserGroupEnum.USER)
-        user_group = await db_session.scalar(stmt)
-
-        assert user_group is not None
+        user_group = await _get_default_user_group(db_session)
 
         user = UserModel.create(
             email=self.user_email,
@@ -151,3 +159,91 @@ class TestRegisterAndActivateEndpoints:
         )
 
         assert response.status_code == 400
+
+
+class TestLoginEndpoint:
+    user_email = "user@example.com"
+    user_password = "1Qazcde3"
+    login_endpoint = "/api/v1/accounts/login"
+
+    async def _login_default_user(self, client: AsyncClient) -> Response:
+        response = await client.post(
+            self.login_endpoint,
+            data={"username": self.user_email, "password": self.user_password},
+        )
+        return response
+
+    async def test_successful_login(
+        self, client: AsyncClient, db_session: AsyncSession
+    ) -> None:
+        user_group = await _get_default_user_group(db_session)
+
+        user = UserModel.create(
+            email=self.user_email,
+            raw_password=self.user_password,
+            group_id=user_group.id,
+        )
+        user.is_active = True
+
+        db_session.add(user)
+        await db_session.commit()
+
+        response = await self._login_default_user(client)
+        assert response.status_code == 200
+
+        assert response.json() == {
+            "refresh_token": "mocked_refresh_token",
+            "access_token": "mocked_access_token",
+        }
+
+        stmt = select(RefreshTokenModel).where(RefreshTokenModel.user_id == user.id)
+        refresh_token = await db_session.scalar(stmt)
+
+        assert refresh_token is not None
+        assert refresh_token.token == "mocked_refresh_token"
+
+    @pytest.mark.parametrize(
+        "email,password",
+        [
+            ("wrong@example.com", "1Qazcde3"),
+            ("user@example.com", "WrongPassword123"),
+            ("wrong@example.com", "WrongPassword123"),
+        ],
+    )
+    async def test_login_invalid_credentials(
+        self, client: AsyncClient, db_session: AsyncSession, email: str, password: str
+    ) -> None:
+        group = await _get_default_user_group(db_session)
+        user = UserModel.create(
+            email=self.user_email,
+            raw_password=self.user_password,
+            group_id=group.id,
+        )
+        user.is_active = True
+        db_session.add(user)
+        await db_session.commit()
+
+        response = await client.post(
+            self.login_endpoint,
+            data={"username": email, "password": password},
+        )
+
+        assert response.status_code == 401
+
+    async def test_login_inactive_user(
+        self, client: AsyncClient, db_session: AsyncSession
+    ) -> None:
+        group = await _get_default_user_group(db_session)
+
+        user = UserModel.create(
+            email=self.user_email,
+            raw_password=self.user_password,
+            group_id=group.id,
+        )
+
+        db_session.add(user)
+        await db_session.commit()
+
+        response = await self._login_default_user(client)
+
+        assert response.status_code == 403
