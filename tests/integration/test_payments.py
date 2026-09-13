9671a9d10697c394f214ada718f2773ef731f5a1
Dmytro <ys33ys33ys55@gmail.com>
2026-09-13 14:47:11 +0300

    test(cart): add integration tests for user cart endpoints

--- tests/integration/test_payments.py
+++ tests/integration/test_payments.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 
 import pytest
 from httpx import AsyncClient
-from sqlalchemy import select, func
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
